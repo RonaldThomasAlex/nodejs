68aec4c671669c46471fe85a6f1f64075a0ba69d
RonaldThomasAlex <ronaldthomas094@gmail.com>
2024-03-28 16:48:50 +0530

    node module system

--- app.js
+++ app.js
@@ -1,57 +1,7 @@
 const http = require("http");
-const fs = require("fs");
 
-function rqListener(req, res) {
-  const url = req.url;
-  const method = req.method;
+const routes = require("./routes");
 
-  if (url === "/") {
-    res.write("<html>");
-    res.write("<head><title>Enter message</title></head>");
-    res.write(
-      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-    );
-    res.write("</html>");
-
-    return res.end();
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    req.on("data", (chunk) => {
-      console.log("chunk", chunk);
-      body.push(chunk);
-    });
-
-    return req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split("=")[1];
-
-      // fs.writeFile("message.text", message);
-      // res.statusCode = 302;
-      // res.setHeader("Location", "/");
-
-      // return res.end();
-
-      fs.writeFile("message.text", message, (err) => {
-        res.statusCode = 302;
-        res.setHeader("Location", "/");
-
-        return res.end();
-      });
-    });
-  }
-
-  res.setHeader("Content-type", "text/html");
-  res.write("<html>");
-  res.write("<head><title>My first page</title></head>");
-  res.write("<body><h1>Hello from Node.js server</h1></body>");
-  res.write("</html>");
-
-  res.end();
-}
-
-const server = http.createServer(rqListener);
+const server = http.createServer(routes);
 
 server.listen(3000);

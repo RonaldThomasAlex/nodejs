50c802ea7383c11a371d6b6b2f120db26a392124
RonaldThomasAlex <ronaldthomas094@gmail.com>
2024-03-28 07:57:02 +0530

    writing to file using fs

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 const http = require("http");
+const fs = require("fs");
 
 function rqListener(req, res) {
-  console.log(req.url, req.method);
+  const url = req.url;
+  const method = req.method;
 
-  //   process.exit();
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write("</html>");
+
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+
+    return res.end();
+  }
 
   res.setHeader("Content-type", "text/html");
   res.write("<html>");
